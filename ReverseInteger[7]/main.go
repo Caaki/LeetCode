69c79152b7d9a17602da65a93b15ee6485384d28
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-01-31 20:01:05 +0100

    fixed

--- ReverseInteger[7]/main.go
+++ ReverseInteger[7]/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"unicode/utf8"
 )
 
 func main() {
-	fmt.Println(reverse(-123))
+	fmt.Println(math.MaxInt32)
+	fmt.Println(reverse2(1534236469))
+}
+
+func reverse2(x int) int {
+	var returnVal int32 = 0
+	negative := x < 0
+	counter := 0
+	for x != 0 {
+		if counter >= 10 {
+			if !negative {
+				if (int64(returnVal)*10 + int64(x%10)) > math.MaxInt32 {
+					return 0
+				}
+			} else {
+				if (int64(returnVal)*10 + int64(x%10)) < math.MinInt32 {
+					return 0
+				}
+			}
+			returnVal = returnVal*10 + int32(x%10)
+			x /= 10
+			counter++
+		} else {
+			returnVal = returnVal*10 + int32(x%10)
+			x /= 10
+			counter++
+		}
+	}
+	return int(returnVal)
 }
 
 func reverse(x int) int {
 	strVal := strconv.Itoa(x)
-	if strVal[0] == '-' {
+	if x < 0 {
 		newStr := ReverseString(strVal[1:])
 		intVal, err := strconv.ParseInt(newStr, 10, 32)
 		if err != nil {
