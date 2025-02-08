3b1e47b6a24bf949284d9be2b026e3946ca3e4a9
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-02-08 23:40:51 +0100

    tryed with recursion but exceedes time limit

--- LongestConsecutiveSequence[128]/main.go
+++ LongestConsecutiveSequence[128]/main.go
@@ -12,44 +12,34 @@ func main(){
 }
 
 func longestConsecutive(nums []int) int{
-  values:= make(map[int][]int)
+  values:= make(map[int]bool)
   longest:=0
   for _,num :=range nums{
     if _,ok:= values[num];ok{
       continue
     }
-    values[num] = []int{num}
-    time.Sleep(1*time.Second)
-    fmt.Println("num is",num,"and it has",values[num])
-    getFirst(values, num,num)
-    getLast(values,num)
-    if longest < len(values[num]){
-      longest = len(values[num])
+    tempCount:=1
+    values[num] = true
+    tempCount +=getFirst(values, num)
+    tempCount+=getLast(values,num)
+    if longest < tempCount{
+      longest =tempCount
     }
   }
   return longest
 }
 
-func getFirst(values map[int][]int, num int, appendTo int){
+func getFirst(values map[int]bool,num int) int{
   if _,ok:= values[num-1];ok{
-    time.Sleep(1*time.Second)
-
-    if values[num-1][len(values[num-1])-1] == num{
-  values[appendTo] = append(values[num-1][:len(values[num-1])],values[appendTo]...)
-    }else{
-    values[appendTo]=append(values[num-1], values[appendTo]...)
-    }
-    fmt.Println(values[appendTo])
-    getFirst(values, values[appendTo][0],appendTo)
+    return 1+getFirst(values, num-1)
   }
+  return 0
 }
 
-func getLast(values map[int][]int, num int){
+func getLast(values map[int]bool, num int) int{
   if _,ok:= values[num+1];ok{
-    time.Sleep(1*time.Second)
-    values[num] = append(values[num],values[num+1]...)
-    fmt.Println(values[num])
-    getLast(values, values[num+1][0])
+    return 1+ getLast(values,num+1)
   }
+  return 0
 }
 
