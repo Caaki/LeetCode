caf3f5415863f2e558427d77e0b4a3f611d48195
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-02-09 12:49:56 +0100

    Finished MinStack

---
+++ MinStack[155]/go.mod
@@ -0,0 +1,3 @@
+module minStack
+
+go 1.23rc1
---
+++ MinStack[155]/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func main(){}
+
+type MinStack struct{
+  stack []int
+  Min []int16
+}
+
+func Constructor() MinStack{
+  minStack:= MinStack{[]int{}, []int16{}}
+  return minStack
+}
+
+func (this *MinStack) Push (val int) {
+  if len(this.stack) <=0{
+    this.stack = append(this.stack,val)
+    this.Min = append(this.Min,0)
+  }else{
+    this.stack = append(this.stack,val)
+    if this.stack[this.Min[len(this.Min)-1]] >= val {
+      this.Min = append(this.Min, int16(len(this.stack)-1))
+    }
+  }
+}
+
+func (this *MinStack) Pop(){
+  if len(this.stack) <=0 {
+  }else{
+    if len(this.stack)-1 == int(this.Min[len(this.Min)-1]){
+      this.Min = this.Min[:len(this.Min)-1]
+      this.stack = this.stack[:len(this.stack)-1]
+    }else{
+      this.stack = this.stack[:len(this.stack)-1]
+    }
+  }
+}
+
+func (this *MinStack) Top() int{
+  if len(this.stack) <= 0 {
+    return 0
+  }else{
+    return this.stack[len(this.stack)-1]
+  }
+}
+
+func (this *MinStack) GetMin() int{
+  if len(this.stack) <=0{
+    return 0
+  }else{
+   return this.stack[this.Min[len(this.Min)-1]]
+  }
+}
