72e164abe953f085aeacc90e584959f18a8e01e0
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-02-09 02:35:33 +0100

    It works but not for all test cases it uses too much memory il try to fix it

--- LongestConsecutiveSequence[128]/main.go
+++ LongestConsecutiveSequence[128]/main.go
@@ -1,45 +1,54 @@
 package main
 
-import (
-
-  "fmt"
-  "time"
-)
-
-func main(){
-
-  fmt.Println(longestConsecutive([]int{0,3,7,2,5,8,4,6,0,1}))
+type Values struct {
+	Lower  []int16
+	Higher []int16
 }
 
-func longestConsecutive(nums []int) int{
-  values:= make(map[int]bool)
-  longest:=0
-  for _,num :=range nums{
-    if _,ok:= values[num];ok{
-      continue
-    }
-    tempCount:=1
-    values[num] = true
-    tempCount +=getFirst(values, num)
-    tempCount+=getLast(values,num)
-    if longest < tempCount{
-      longest =tempCount
-    }
-  }
-  return longest
+type Values2 struct {
+	Lowest  int
+	Highest int
 }
 
-func getFirst(values map[int]bool,num int) int{
-  if _,ok:= values[num-1];ok{
-    return 1+getFirst(values, num-1)
-  }
-  return 0
+func main() {
+
 }
 
-func getLast(values map[int]bool, num int) int{
-  if _,ok:= values[num+1];ok{
-    return 1+ getLast(values,num+1)
-  }
-  return 0
+func longestConsecutive(nums []int) int {
+	values := make(map[int16]*Values)
+	longest := 0
+	for _, num := range nums {
+		if _, ok := values[int16(num)]; ok {
+			continue
+		}
+		val := Values{Higher: []int16{}, Lower: []int16{}}
+		values[int16(num)] = &val
+		getFirst(values, int16(num), int16(num))
+		getLast(values, int16(num), int16(num))
+		if longest < len(values[int16(num)].Higher)+len(values[int16(num)].Lower)+1 {
+			longest = len(values[int16(num)].Higher) + len(values[int16(num)].Lower) + 1
+		}
+	}
+	return longest
 }
 
+func getFirst(values map[int16]*Values, num int16, appendTo int16) {
+	if _, ok := values[num-1]; ok {
+		values[appendTo].Lower = append([]int16{num - 1}, values[appendTo].Lower...)
+		if len(values[num-1].Lower) > 0 {
+			values[appendTo].Lower = append(values[num-1].Lower, values[appendTo].Lower...)
+		}
+
+		getFirst(values, values[appendTo].Lower[0], appendTo)
+	}
+}
+
+func getLast(values map[int16]*Values, num int16, appendTo int16) {
+	if _, ok := values[num+1]; ok {
+		values[appendTo].Higher = append(values[appendTo].Higher, num+1)
+		if len(values[num+1].Higher) > 0 {
+			values[appendTo].Higher = append(values[appendTo].Higher, values[num+1].Higher...)
+		}
+		getLast(values, values[appendTo].Higher[len(values[appendTo].Higher)-1], appendTo)
+	}
+}
