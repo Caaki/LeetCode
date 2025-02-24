1c15be02383dff7248dafbcd5d5e7284b25bbedc
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-02-25 00:26:01 +0100

    i got something

--- MostProfitablePathInATree[2467]/main.go
+++ MostProfitablePathInATree[2467]/main.go
@@ -1,46 +1,79 @@
 package main
 
+func main() {}
 
-func main(){}
+func mostProfitablePath(edges [][]int, bob int, amount []int) int {
+	if len(edges) == 1 {
+		return amount[0]
+	}
+	alieceMax := amount[0]
+	allPaths := findEdgeNumbers(edges, bob)
+	for _, v := range allPaths {
+		current := calculateMax(v, bob, amount)
+		if current > alieceMax {
+			alieceMax = current
+		}
+	}
 
-type Paths struct{
-  path []int
-  isBob bool
+	return alieceMax
 }
 
-func mostProfitablePath(edges [][]int, bob int, amount []int) int{
-  if len(edges) ==1{
-      return amount[0]
-    }
-  alieceMax := amount[0]
-  alieceCurrent:=amount[0]
-  amount[bob]=0
-
-
-  return alieceMax
+type Paths struct {
+	path   []int
+	isBob  bool
+	bobPos int
 }
 
-func findEdgeNumbers(all [][]int) map[int][]Paths{
-  paths := map[int]Paths{}
+func findEdgeNumbers(all [][]int, bob int) map[int]Paths {
+	paths := map[int]Paths{}
 
-  currentRoot := all[0][0]
-  paths[all[0][0]] = Paths{path:[]int{all[0][0]},isBob:false}
-  for i:=0;i < len(all) ; i++{
-    paths[all[i][1]].path = append(paths[all[i][0]].path,all[i][1])
-    if currentRoot!=all[i][0]{
-      delete(paths, currentRoot)
-      currentRoot=all[i][1]
-    }
-  }
-  return paths
+	currentRoot := all[0][0]
+	paths[all[0][0]] = Paths{path: []int{currentRoot}, isBob: false}
+	for i := 0; i < len(all); i++ {
+		parent, child := all[i][0], all[i][1]
+		if child == bob {
+			paths[child] = Paths{path: append(paths[parent].path, child), isBob: true, bobPos: len(paths[parent].path)}
+		} else {
+			paths[child] = Paths{path: append(paths[parent].path, child), isBob: paths[parent].isBob}
+		}
+		if currentRoot != parent {
+			delete(paths, currentRoot)
+			currentRoot = child
+		}
+	}
+	return paths
 }
 
-func recursiveTraversal(edges [][]int,current int, aliece int,bob *int, amount *[]int, max *int){
+func calculateMax(paths Paths, bob int, amount []int) int {
+	maxAliece := amount[0]
+	current := amount[0]
+	if !paths.isBob {
+		for _, v := range paths.path {
+			current += amount[v]
+			if current > maxAliece {
+				maxAliece = current
+			}
+		}
+		return maxAliece
+	} else {
+		bobPosition := paths.bobPos
+		for i := 0; i < len(paths.path); i++ {
+			if bobPosition >= 0 {
+				if bobPosition == i {
+					amount[i] = amount[i] / 2
+				}
+				amount[bob] = 0
+				current += amount[paths.path[i]]
+				if current > maxAliece {
+					maxAliece = current
+				}
+			} else {
+				current += amount[paths.path[i]]
+				if current > maxAliece {
+					maxAliece = current
+				}
+			}
+		}
+		return maxAliece
+	}
 }
-
-
-//[0] [1] 
-//[1] [2,3]
-//[3] [4]
-
-
