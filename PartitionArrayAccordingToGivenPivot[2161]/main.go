14ae8f76f13282a5ce52f30f1c7e905ce8d3d5e8
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2025-03-03 19:11:31 +0100

    finished Partition Array According to Given Pivot

--- PartitionArrayAccordingToGivenPivot[2161]/main.go
+++ PartitionArrayAccordingToGivenPivot[2161]/main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"slices"
+)
 
-func main(){}
+func main() {}
 
-func pivotArray(nums []int, pivot int) int{
+func pivotArray(nums []int, pivot int) []int {
+	less := []int{}
+	greater := []int{}
+	equal := []int{}
+
+	for _, v := range nums {
+		if v == pivot {
+			equal = append(equal, v)
+			continue
+		}
+		if v > pivot {
+			greater = append(greater, v)
+			continue
+		}
+		less = append(less, v)
+	}
+
+	return slices.Concat(less,
+		equal,
+		greater,
+	)
 }
