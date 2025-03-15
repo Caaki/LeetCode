a4215cdfa3323e8a6344a09ab4a8f8cea7ae0a38
Caaki <95718662+Caaki@users.noreply.github.com>
2025-03-15 23:13:41 +0100

    finished houserobber4

--- HouseRobber4[2560]/main.go
+++ HouseRobber4[2560]/main.go
@@ -1,1 +1,32 @@
 package main
+
+import "math"
+
+func canStealKHouses(nums []int, k, capability int) bool {
+	count := 0
+	for i := 0; i < len(nums); {
+		if nums[i] <= capability {
+			count++
+			i += 2
+		} else {
+			i++
+		}
+	}
+	return count >= k
+}
+
+func minCapability(nums []int, k int) int {
+	l := 1
+	r := math.MaxInt32
+
+	for l < r {
+		mid := l + (r-l)/2
+		if canStealKHouses(nums, k, mid) {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return l
+}
