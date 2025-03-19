5e5b60b4f95c9e84325200b96cba0c315286d1c2
Caaki <nikolalazin7@gmail.com>
2025-03-19 23:48:54 +0100

    Finished minimum operations to make binary elemnts equal to 1

--- MinimumOperationsToMakeBinaryArrayElementsEqualTo1[3191]/main.go
+++ MinimumOperationsToMakeBinaryArrayElementsEqualTo1[3191]/main.go
@@ -1,21 +1,34 @@
 package main
 
+import(
+  "fmt"
+)
 
 func main() {
-  v:=1
-  changeVal(&v)
+  fmt.Println(minOperations([]int{0,1,1,1,0,0}))
 }
-func minOperations(nums []int) int{
 
+func minOperations(nums []int) int{
   count:=0
 
   l,r:=0,2
   for r<len(nums) {
-    
+    if nums[l] == 0{
+      changeVal(nums,l,r)
+      count++
+    }
+    l++
+    r++
+  }
+  if nums[l-1] != nums[l] || nums[r-1] != nums[l-1]{
+    return -1
   }
   return count
 }
 
-func changeVal(v *int){
-  *v = *v^(1 << 0)
+func changeVal(nums []int, l,r int){
+  for l<=r{
+    nums[l] = nums[l]^(1 << 0)
+    l++
+  }
 }
