61378383df85b5a73b96902ce978edfa1a92902f
Caaki <nikolalazin7@gmail.com>
2025-03-24 00:04:38 +0100

    Fixed add binary to be faster by using a rune array insted of string

--- AddBinary[67]/main.go
+++ AddBinary[67]/main.go
@@ -5,7 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println(addBinary("11101", "1101001"))
 }
-
 func addBinary(a, b string) string {
 
 	if len(b) > len(a) {
@@ -17,40 +16,40 @@ func addBinary(a, b string) string {
 	for i := len(b); i < len(a); i++ {
 		b = "0" + b
 	}
-	result := ""
+	result := make([]rune, len(a))
 	remainder := false
 	for i := len(a) - 1; i >= 0; i-- {
 		if b[i] != a[i] {
 			if remainder {
-				result = "0" + result
+				result[i] = '0'
 				continue
 			}
-			result = "1" + result
+			result[i] = '1'
 			continue
 		}
 		if b[i] == '0' {
 			if remainder {
-				result = "1" + result
+				result[i] = '1'
 				remainder = false
 				continue
 			}
-			result = "0" + result
+			result[i] = '0'
 			continue
 		}
 		if remainder {
 			remainder = true
-			result = "1" + result
+			result[i] = '1'
 			continue
 		}
-		result = "0" + result
+		result[i] = '0'
 		remainder = true
 	}
 
 	if remainder {
 		if a[0] == '1' {
-			return "1" + result
+			return "1" + string(result)
 		}
-		return "1" + result[1:]
+		return "1" + string(result[1:])
 	}
-	return result
+	return string(result)
 }
