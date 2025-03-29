698a34796ca03c2beae2a0841fdfadea999914ba
Caaki <nikolalazin7@gmail.com>
2025-03-30 00:37:44 +0100

    Something is still missing

--- MaximumNumberOfPointsFromGridQueries[2503]/main.go
+++ MaximumNumberOfPointsFromGridQueries[2503]/main.go
@@ -41,26 +41,34 @@ func maxPoints(grid [][]int, queries []int) []int {
 	var toVisit = &minHeap{}
 	heap.Init(toVisit)
 	heap.Push(toVisit, cell{i: 0, j: 0, val: grid[0][0]})
-	visited := make(map[cell]bool, 0)
+	visited := make([][]bool, len(grid))
+	for i := range visited {
+		visited[i] = make([]bool, len(grid[0]))
+	}
+	directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 
 	count := 0
 	for _, q := range qSorted {
 		for toVisit.Len() > 0 && grid[(*toVisit)[0].i][(*toVisit)[0].j] < q {
 
 			cordinates := heap.Pop(toVisit).(cell)
-			if _, ok := visited[cordinates]; !ok {
-				count++
-			}
 
-			if cordinates.i+1 < len(grid) && !visited[cordinates] {
-				heap.Push(toVisit, cell{i: cordinates.i + 1, j: cordinates.j, val: grid[cordinates.i+1][cordinates.j]})
+			count++
+			for _, dir := range directions {
+				x, y := cordinates.i+dir[0], cordinates.j+dir[1]
+				if x >= 0 && x < len(grid) && y >= 0 && y < len(grid[0]) && !visited[x][y] {
+					visited[x][y] = true
+					heap.Push(toVisit, cell{x, y, grid[x][y]})
+				}
 			}
-			if cordinates.j+1 < len(grid[0]) && !visited[cordinates] {
-				heap.Push(toVisit, cell{i: cordinates.i, j: cordinates.j + 1, val: grid[cordinates.i][cordinates.j+1]})
-			}
-			visited[cordinates] = true
 		}
-		answers[order[q]] = count
+		if count > 1 {
+			answers[order[q]] = count - 1
+
+		} else {
+			answers[order[q]] = count
+
+		}
 	}
 	return answers
 }
