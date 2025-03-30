7193f32dc535d2151c2f7dff13d2617662a35930
Caaki <nikolalazin7@gmail.com>
2025-03-31 00:29:10 +0200

    Almost finished

--- PartitionLabels[763]/main.go
+++ PartitionLabels[763]/main.go
@@ -1,8 +1,43 @@
 package main
 
+import "sort"
+
 func main() {}
 
+type Val struct {
+	start int
+	end   int
+}
+
+type sortByStart []Val
+
+func (s sortByStart) Len() int {
+	return len(s)
+}
+
+func (s sortByStart) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortByStart) Less(i, j int) bool {
+	return s[i].start < s[j].start
+}
+
 func partitionLabels(s string) []int {
+	sVals := make(sortByStart, 26, 26)
+	for i, _ := range sVals {
+		sVals[i] = Val{start: -1, end: -1}
+	}
+	for i, v := range s {
+		if sVals[v-'a'].start == -1 {
+			sVals[v-'a'].start = i
+			sVals[v-'a'].end = i
+		} else {
+			sVals[v-'a'].end = i
+		}
+	}
+	answers := make([]int, 0)
+	sort.Sort(sVals)
 
 	return []int{}
 }
