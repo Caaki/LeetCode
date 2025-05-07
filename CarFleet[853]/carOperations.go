a469e47fc92f802215f1dfaf515b9d424a8c523b
Caaki <nikolalazin7@gmail.com>
2025-05-07 02:47:16 +0200

    Optimised Car fleet to use int32 for memory efficiency

--- CarFleet[853]/carOperations.go
+++ CarFleet[853]/carOperations.go
@@ -1,44 +1,42 @@
 package main
 
-
-
 type Car struct {
-	position int
-	speed    int
+  position int32
+  speed    int32
 }
 
 func (c Car) ArrivalTime() float32 {
-	return float32(t-c.position) / float32(c.speed)
+  return float32(t-c.position) / float32(c.speed)
 }
 
 type carSorting []Car
 
 func (c carSorting) Len() int {
-	return len(c)
+  return len(c)
 }
 
 func (c carSorting) Swap(i, j int) {
-	c[i], c[j] = c[j], c[i]
+  c[i], c[j] = c[j], c[i]
 }
 func (c carSorting) Less(i, j int) bool {
-	return c[i].position > c[j].position
+  return c[i].position > c[j].position
 }
 
 type CarStack struct {
-	Cars []Car
+  Cars []Car
 }
 
 func (s *CarStack) Len() int {
-	return len(s.Cars)
+  return len(s.Cars)
 }
 func (s *CarStack) Push(c Car) {
-	s.Cars = append(s.Cars, c)
+  s.Cars = append(s.Cars, c)
 }
 
 func (s *CarStack) ArrivalTime() float32 {
-	if s.Len() <= 0 {
-		return -1
-	}
-	c := s.Cars[s.Len()-1]
-	return c.ArrivalTime()
+  if s.Len() <= 0 {
+    return -1
+  }
+  c := s.Cars[s.Len()-1]
+  return c.ArrivalTime()
 }
--- CarFleet[853]/main.go
+++ CarFleet[853]/main.go
@@ -4,14 +4,14 @@ import "sort"
 
 func main() {}
 
-var t int = 0
+var t int32
 
 func carFleet(target int, position []int, speed []int) int {
-	t = target
+	t = int32(target)
 	cars := make([]Car, len(position))
 
 	for i := range len(position) {
-		cars[i] = Car{position: position[i], speed: speed[i]}
+		cars[i] = Car{position: int32(position[i]), speed: int32(speed[i])}
 	}
 
 	sort.Sort(carSorting(cars))
