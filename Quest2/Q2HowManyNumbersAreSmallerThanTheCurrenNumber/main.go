849711fa2870065cc5f845e945452f98d70cd7d6
Caaki <nikolalazin7@gmail.com>
2026-08-02 00:53:35 +0000

    first try it can be improved, problem name [How many numbers are smaller than the current number]

--- Quest2/Q2HowManyNumbersAreSmallerThanTheCurrenNumber/main.go
+++ Quest2/Q2HowManyNumbersAreSmallerThanTheCurrenNumber/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "sort"
-
 /*
 Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
 That is, for each nums[i]
@@ -12,13 +10,28 @@ Return the answer in an array.
 
 func main(){}
 
+type N struct {
+	num, count int
+}
 
 func smallerNumbersThanCurrent(nums []int) []int {
-	
-	cpArray := nums
-	sort.Ints(cpArray)
-	valPlace := make(map[int]int, len(nums))
-	c = cpArray
-	for _,v := range()
+	nCount := make(map[int] int)
 
+	for _,v := range nums{
+		nCount[v]++
+	}
+	
+	rValues := make([]int,len(nums))
+	
+	for i,cur := range nums{
+		count := 0
+		for k,v :=range nCount{
+			if cur > k{
+				cur+= v
+			}
+		}
+		rValues[i] = count
+	}
+
+	return rValues
 }
